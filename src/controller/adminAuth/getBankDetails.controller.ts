2816dece12471650091cdd821ad92cc65cab57e3
Aadarsh <heyitsaadarsh@gmail.com>
2022-04-10 23:51:22 +0530

    Changing response type

--- src/controller/adminAuth/getBankDetails.controller.ts
+++ src/controller/adminAuth/getBankDetails.controller.ts
@@ -10,7 +10,19 @@ export const getBankDetailsController = async (_: Request, res: Response) => {
       return;
     }
 
-    res.status(201).json({ error: false, data: [bankDetails[0]] });
+    const stringbalance = JSON.stringify(bankDetails[0].total_bank_balance);
+    const bank_bal = JSON.parse(stringbalance);
+    const { $numberDecimal } = bank_bal;
+
+    res.status(201).json({
+      error: false,
+      data: [
+        {
+          total_bank_balance: +$numberDecimal,
+          total_number_of_user: bankDetails[0].number_of_users,
+        },
+      ],
+    });
     return;
   } catch (err) {
     res.status(201).json({ error: true, data: { message: [err.message] } });
